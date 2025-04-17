06b036f869591e5e77742c348877cd13dd108d5b
Itz-Agasta <rupamgolui69@gmail.com>
2025-04-17 23:38:33 +0530

    Add FastAPI setup with CORS middleware and root endpoint

---
+++ backend/app/main.py
@@ -0,0 +1,34 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+# from core.config import get_settings # Experimental
+from routes.analyze import router as analyzer_router
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+app = FastAPI()
+
+allowed_origin = os.getenv("FRONTEND_ORIGIN_URL")
+if allowed_origin:
+    origins =  allowed_origin   # --Production
+else:
+    origins = [                 # --Development
+        "http://localhost",
+        "http://localhost:5173"
+    ]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/")
+def root():
+    return {"message": "Hello from vyse",
+            "allowed_origins": origins
+      }
+
+app.include_router(analyzer_router, prefix="/analyze", tags=["analyze"])
